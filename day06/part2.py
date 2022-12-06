e697af005717c5822b045e33ee34d06a584c06c7
stickperson <joe.meissler@gmail.com>
2022-12-05 21:11:11 -0800

    day6

---
+++ day06/part2.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    unique_chars = 14
+    for idx in range(unique_chars, len(s)):
+        unique = len(set(s[idx-unique_chars:idx]))
+        if unique == unique_chars:
+            return idx
+
+
+INPUT_S = '''\
+mjqjpqmgbljsphdztnvjfqwrcgsmlb
+'''
+EXPECTED = 19
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
